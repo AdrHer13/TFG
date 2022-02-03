b7ca82820a7f50a6f97d7d9e7f991d564f6c7abb
Adrian Heras <adrianherasreche@gmail.com>
2022-02-03 17:44:38 +0100

    Inicio del planteamiento de las clases para las trazas

---
+++ Catan Trainer/TraceLoader/TraceLoader.py
@@ -0,0 +1,39 @@
+from TraceLoader.Interpreter import Interpreter
+
+
+class TraceLoader:
+    current_trace = {}
+    interpreter = Interpreter()
+
+    def __init__(self):
+        return
+
+    def export_to_file(self):
+        """
+        Función que exporta a formato JSON la variable current_trace
+        :return: None
+        """
+        return
+
+    def import_to_file(self):
+        """
+        Función que carga en la variable current_trace un archivo JSON
+        :return: None
+        """
+        return
+
+    def play_trace(self):
+        """
+        Función que llama a las funciones del intérprete para ejecutar la traza
+        :return: None
+        """
+        return
+
+    def save_to_trace(self):
+        """
+        TODO
+        Función que permite añadir datos a la traza. Requiere de especificar jugador, turno y demás
+         o los recogerá automáticamente cuando se le llame
+        :return:
+        """
+        return

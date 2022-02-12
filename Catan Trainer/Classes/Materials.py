12a7bfc772a1cdedebfc6fe3ef8af1b416fda85c
Adrian Heras <adrianherasreche@gmail.com>
2022-02-13 00:16:21 +0100

    Clase materiales completada

--- Catan Trainer/Classes/Materials.py
+++ Catan Trainer/Classes/Materials.py
@@ -8,51 +8,68 @@ class Materials:
     wood = 0
     wool = 0
 
-    def __init__(self):  # , default=True):
-        # if (default):
-        #     self.cereal = 0
-        #     self.mineral = 0
-        #     self.clay = 0
-        #     self.wood = 0
-        #     self.wool = 0
-        #
-        # else:
-        #     self.cereal = None
-        #     self.mineral = None
-        #     self.clay = None
-        #     self.wood = None
-        #     self.wool = None
-
+    def __init__(self):
         return
 
     ##### getters #####
     def get_cereal(self):
-        return
+        return self.cereal
 
     def get_mineral(self):
-        return
+        return self.mineral
 
     def get_clay(self):
-        return
+        return self.clay
 
     def get_wood(self):
-        return
+        return self.wood
 
     def get_wool(self):
-        return
+        return self.wool
 
     ##### setters #####
-    def set_cereal(self):
+    def set_cereal(self, amount=int):
+        self.cereal = amount
         return
 
-    def set_mineral(self):
+    def set_mineral(self, amount=int):
+        self.mineral = amount
         return
 
-    def set_clay(self):
+    def set_clay(self, amount=int):
+        self.clay = amount
         return
 
-    def set_wood(self):
+    def set_wood(self, amount=int):
+        self.wood = amount
         return
 
-    def set_wool(self):
+    def set_wool(self, amount=int):
+        self.wool = amount
+        return
+
+    ##### adders #####
+    def add_cereal(self, amount):
+        cereal = self.get_cereal() + amount
+        self.set_cereal(cereal)
+        return
+
+    def add_mineral(self, amount):
+        mineral = self.get_mineral() + amount
+        self.set_mineral(mineral)
+        return
+
+    def add_clay(self, amount):
+        clay = self.get_clay() + amount
+        self.set_clay(clay)
+        return
+
+    def add_wood(self, amount):
+        wood = self.get_wood() + amount
+        self.set_wood(wood)
+        return
+
+    def add_wool(self, amount):
+        wool = self.get_wool() + amount
+        self.set_wool(wool)
         return

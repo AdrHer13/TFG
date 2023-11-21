80344649bfe3b506459ed1e4c15b8df8da6533af
pasjia <alexpastjim@gmail.com>
2023-11-21 21:16:13 +0100

    Creado test_turn_manager

---
+++ Catan Trainer/Tests/test_turn_manager.py
@@ -0,0 +1,41 @@
+# No he hecho nada
+
+from Managers.TurnManager import TurnManager
+
+class TestTurnManager:
+
+    def test_get_turn(self):
+
+        return
+
+    def test_get_whose_turn_is_it(self):
+
+        return
+
+    def test_get_phase(self):
+
+        return
+
+    def test_get_round(self):
+
+        return
+
+    #############################
+    ########## setters ##########
+    #############################
+    def test_set_turn(self, turn=0):
+
+        return
+
+    def test_set_whose_turn_is_it(self):
+
+        return
+
+    def test_set_phase(self):
+
+        return
+
+    def test_set_round(self):
+
+        return
+

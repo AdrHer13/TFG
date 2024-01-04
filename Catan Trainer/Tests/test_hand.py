b42a83c68ed8d6701050ab12590f6087a3bd1869
pasjia <alexpastjim@gmail.com>
2024-01-04 16:06:31 +0100

    Cositas del test de mano
    Los remove y add no devuelven nada.
    He quitado lo de mc porque solo se usan 1 vez y para eso que sea todo igual

--- Catan Trainer/Tests/test_hand.py
+++ Catan Trainer/Tests/test_hand.py
@@ -1,4 +1,3 @@
-from Classes.Constants import MaterialConstants as mc
 from Classes.Hand import *
 
 
@@ -10,18 +9,18 @@ class TestHand:
         assert hand.get_total() == 0
 
         # Comprobamos que add_material añade materiales correctamente
-        hand.add_material(mc.CEREAL, 1)
-        hand.add_material(mc.MINERAL, 1)
-        hand.add_material(mc.CLAY, 1)
-        hand.add_material(mc.WOOD, 1)
-        hand.add_material(mc.WOOL, 1)
-        assert hand.get_cereal() == 1 and hand.get_mineral() == 1 and hand.get_clay() == 1 and hand.get_wood() == 1 and \
-               hand.get_wool() == 1 and hand.get_total() == 5
+        hand.add_material(0, 1)
+        hand.add_material(1, 1)
+        hand.add_material(3, 1)
+        hand.add_material(4, 1)
+        hand.add_material(5, 1)
+        assert hand.get_cereal() == 1 and hand.get_mineral() == 1 and hand.get_clay() == 1 and \
+               hand.get_wood() == 1 and hand.get_wool() == 1 and hand.get_total() == 5
 
         # Comprobamos que la versión por lista funciona correctamente también
         hand.add_material([0, 1, 2, 3, 4], 1)
-        assert hand.get_cereal() == 2 and hand.get_mineral() == 2 and hand.get_clay() == 2 and hand.get_wood() == 2 and \
-               hand.get_wool() == 2 and hand.get_total() == 10
+        assert hand.get_cereal() == 2 and hand.get_mineral() == 2 and hand.get_clay() == 2 and \
+               hand.get_wood() == 2 and hand.get_wool() == 2 and hand.get_total() == 10
 
         # Comprobamos que remove_material elimina materiales correctamente
         hand.remove_material(0, 1)
@@ -29,20 +28,20 @@ class TestHand:
         hand.remove_material(2, 1)
         hand.remove_material(3, 1)
         hand.remove_material(4, 1)
-        assert hand.get_cereal() == 1 and hand.get_mineral() == 1 and hand.get_clay() == 1 and hand.get_wood() == 1 and \
-               hand.get_wool() == 1 and hand.get_total() == 5
+        assert hand.get_cereal() == 1 and hand.get_mineral() == 1 and hand.get_clay() == 1 and \
+               hand.get_wood() == 1 and hand.get_wool() == 1 and hand.get_total() == 5
 
         # Comprobamos que la versión por lista funciona correctamente también
         hand.remove_material([0, 1, 2, 3, 4], 1)
         assert hand.get_total() == 0
 
         # Comprobamos si se lanza la excepción al tener menos materiales de los posibles
-        assert hand.remove_material(0, 1) == False
+        assert hand.remove_material(0, 1)
         # Comprobamos qué pasa si se resta 0
-        assert hand.remove_material(0, 0) == True
+        assert hand.remove_material(0, 0)
 
         # Comprobamos qué pasa si se elimina un material pese a tener al menos 1
         hand.add_material(0, 1)
-        assert hand.remove_material(0, 1) == True
+        assert hand.remove_material(0, 1)
 
         return

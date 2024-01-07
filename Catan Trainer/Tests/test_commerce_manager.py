ec54e5e5daa3754e38eb40cadae818ec21bd306e
pasjia <alexpastjim@gmail.com>
2024-01-07 18:44:41 +0100

    Cositas del test del commerce manager

--- Catan Trainer/Tests/test_commerce_manager.py
+++ Catan Trainer/Tests/test_commerce_manager.py
@@ -8,13 +8,21 @@ class TestCommerceManager:
         hand = Hand()
         hand.add_material(0, 6)
 
+        # Comercio sin puerto
         commerce_manager.trade_without_harbor(hand, 0, 1)
         assert hand.get_from_id(0) == 2 and hand.get_from_id(1) == 1
 
+        # Comercio con puerto
         hand.add_material(2, 5)
         commerce_manager.trade_through_harbor(hand, 2, 3)
         assert hand.get_from_id(2) == 2 and hand.get_from_id(3) == 1
 
+        # Comercio con un puerto especializado
         hand.add_material(4, 4)
         commerce_manager.trade_through_special_harbor(hand, 4, 0)
         assert hand.get_from_id(4) == 2 and hand.get_from_id(0) == 3
+
+
+if __name__ == '__main__':
+    test = TestCommerceManager()
+    test.test_commerce_manager()

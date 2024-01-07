bb7191c6f28ccf1fc085f19d48182dc1188cb959
pasjia <alexpastjim@gmail.com>
2024-01-07 20:23:13 +0100

    Epicardo el código del test re-hecho
    Es posible que la imagen del test ya no sea acertada, pero es muy similar

--- Catan Trainer/Tests/LongestRoadTest.py
+++ Catan Trainer/Tests/LongestRoadTest.py
@@ -4,50 +4,49 @@ from Managers.GameManager import *
 class LongestRoadTest:
     def longest_road_test(self):
         gm = GameManager(for_test=True)
+        longest_road = {'longest_road': 4, 'player': -1}
 
-        gm.board.nodes[0]['player'] = 0
-        gm.board.build_road(0, 0, 1)
-        gm.board.build_road(0, 1, 2)
-        gm.board.build_road(0, 0, 8)
-        gm.board.build_road(0, 8, 9)
-        gm.board.build_road(0, 9, 10)
+        for case in range(4):
+            if case == 0:  # Caso 1: Aún no hay nadie con 5 o más caminos
+                gm.board.nodes[0]['player'] = 0
+                gm.board.build_road(0, 0, 1)
+                gm.board.build_road(0, 1, 2)
+                gm.board.build_road(0, 0, 8)
 
-        gm.board.build_town(0, 10)
-        gm.board.build_road(0, 10, 11)
-        gm.board.build_road(0, 11, 21)
+            if case == 1:  # Caso 2: Hay un jugador con 5 o más caminos
+                gm.board.build_road(0, 8, 9)
+                gm.board.build_road(0, 9, 10)
 
-        gm.board.build_road(0, 20, 21)
-        gm.board.build_road(0, 21, 22)
-        gm.board.build_road(0, 22, 23)
-        gm.board.build_town(0, 20)
+            if case == 2:  # Caso 3: Otro jugador tiene igual cantidad de caminos que el que poseía el camino más largo
+                gm.board.nodes[17]['player'] = 1
+                gm.board.build_road(1, 17, 18)
+                gm.board.build_road(1, 18, 19)
+                gm.board.build_road(1, 19, 20)
+                gm.board.build_road(1, 20, 31)
+                gm.board.build_road(1, 31, 32)
 
-        gm.board.nodes[47]['player'] = 0
-        gm.board.build_road(0, 47, 39)
+            if case == 3:  # Caso 4: Otro jugador tiene más caminos que el que poseía el camino más largo
+                gm.board.build_road(1, 32, 33)
+                gm.board.build_road(1, 33, 34)
+                gm.board.build_road(1, 34, 35)
 
-        gm.board.nodes[17]['player'] = 1
-        gm.board.build_road(1, 17, 18)
-        gm.board.build_road(1, 18, 19)
-        gm.board.build_road(1, 19, 20)
-        gm.board.build_road(1, 20, 31)  # No debería de poder construirla, pero deja, de momento es conveniento
-        gm.board.build_road(1, 31, 32)
-        gm.board.build_road(1, 32, 33)
-        gm.board.build_road(1, 33, 34)
-        gm.board.build_road(1, 34, 35)
-        gm.board.build_town(1, 35)
-        gm.board.build_road(1, 35, 24)
-        gm.board.build_road(1, 24, 25)
-        gm.board.build_road(1, 25, 26)
-        gm.board.build_road(1, 26, 37)
-        gm.board.build_road(1, 36, 37)  # Si está activo este, gana J2, si no gana J1. Algoritmo va
+            for node in gm.board.nodes:
+                longest_road_obj = gm.longest_road_calculator(node, 1, longest_road, -1, [node['id']])
 
-        gm.board.build_road(1, 33, 22)
+                if longest_road_obj['longest_road'] >= longest_road['longest_road']:
+                    real_longest_road = longest_road_obj
 
-        real_longest_road = {'longest_road': 5, 'player': -1}
-        for node in gm.board.nodes:
-            longest_road_obj = gm.longest_road_calculator(node, 1, {'longest_road': 0, 'player': -1}, -1, [node['id']])
+            if case == 0:  # Caso 1: Aún no hay nadie con 5 o más caminos
+                assert longest_road == {'longest_road': 4, 'player': -1}
 
-            if longest_road_obj['longest_road'] > real_longest_road['longest_road']:
-                real_longest_road = longest_road_obj
+            if case == 1:  # Caso 2: Hay un jugador con 5 o más caminos
+                assert longest_road == {'longest_road': 5, 'player': 0}
+
+            if case == 2:  # Caso 3: Otro jugador alcanza la misma cantidad de caminos que el que tiene la carretera
+                assert longest_road == {'longest_road': 5, 'player': 0}
+
+            if case == 3:  # Caso 4: Otro jugador tiene más caminos que el que poseía el camino más largo
+                assert longest_road == {'longest_road': 8, 'player': 1}
         return
 
 
